de88637266f009b226e880b568902a4c939182cd
Azizb750 <azizb750@gmail.com>
2018-09-22 12:25:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# android_vendor_xiaomi_hermes
+
+vendor AOSP 6.0 22.09
---
+++ hermes-vendor.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/vendor,system/vendor)
