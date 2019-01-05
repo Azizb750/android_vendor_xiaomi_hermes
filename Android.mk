20659947d0583d67681e03d3bb03f046d41fae50
Aziz Bastosun <azizb750@gmail.com>
2019-01-05 11:35:11 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,11 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libmtkplayer
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/vendor/lib/libmtkplayer.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-include $(BUILD_PREBUILT)
+$(info [Decker] copying DP Framework proprietary blobs)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libdpframework
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX := .so
+#LOCAL_MULTILIB := both
+#LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libdpframework.so
+#LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
+#include $(BUILD_PREBUILT)
